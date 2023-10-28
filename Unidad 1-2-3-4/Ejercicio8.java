2af955fa58b65b7335e76e2771462e16b1d3a979
PaulaAtencia <patebar593@g.educaand.es>
2023-10-28 17:31:21 +0200

    Corregido

--- Unidad 1-2-3-4/Ejercicio8.java
+++ Unidad 1-2-3-4/Ejercicio8.java
@@ -1,12 +1,13 @@
-public class Main {
-    
-    public class Ejercicio8_1 {
-        public static void main(String[] args) {
-          
-          System.out.println("*********");
-          System.out.println(" *     *");
-          System.out.println("  *   *");
-          System.out.println("   * *");
-          System.out.println("    *");
-        }
-      }
+public class Ejercicio8 {
+
+  public class Ejercicio8_1 {
+
+    public static void main(String[] args) {
+      System.out.println("*********");
+      System.out.println(" *     *");
+      System.out.println("  *   *");
+      System.out.println("   * *");
+      System.out.println("    *");
+    }
+  }
+}
