d8fb322d8a1c7b3931eee68cd0a250a841996daa
PaulaAtencia <patebar593@g.educaand.es>
2023-11-02 01:12:24 +0100

    REPASO

--- RepasoExamen03-10/Ejercicioentrada.java
+++ RepasoExamen03-10/Ejercicioentrada.java
@@ -1,43 +1,44 @@
 import java.util.Scanner;
 
 public class Ejercicioentrada {
-    public static void main(String[] args) {
-        System.out.println("Inserta el precio de la entrada general");
-        double precioGeneral;
-        Scanner s = new Scanner(System.in);
-        precioGeneral = s.nextDouble();
 
-        //Menú
-        System.out.println ("Elige tu entrada: Palco o gallinero");
-        String eleccion = s.nextLine();
-        
-        //Menú 2
-        System.out.println ("Elige entre descuento, precio normal o sobreprecio con el 1,2 o 3");
-        int opcion = s.nextInt();
+  public static void main(String[] args) {
+    System.out.println("Inserta el precio de la entrada general");
+    double precioGeneral;
+    Scanner s = new Scanner(System.in);
+    precioGeneral = s.nextDouble();
 
-        double porcentaje;
-        if(eleccion == "palco")
-        {
-            porcentaje = 0.1; 
-        } 
-        else 
-        {
-            porcentaje = 0.05;
-        }
-        double resultado;
-        if(opcion == 1)
-        {
-            resultado = precioGeneral - precioGeneral * porcentaje;
-        }
+    //Menú
+    System.out.println("Elige tu entrada 1 o 2");
+    int eleccion = s.nextInt();
 
-        else if (opcion == 3)
-        {
-            resultado = precioGeneral + precioGeneral * porcentaje;
-        }
-        else 
-        {
-            resultado = precioGeneral
-        }
-        System.out.println ("El precio de la entrada es:" + resultado );
-        s.close();
+    //Menú 2
+    System.out.println("Elige entre descuento, precio normal o sobreprecio con el 1,2 o 3");
+    int opcion = s.nextInt();
+
+    double porcentaje;
+    if (eleccion == 1)
+    {
+      porcentaje = 0.1;
+    }
+    else
+    {
+      porcentaje = 0.05;
+    }
+    double resultado;
+    if (opcion == 1)
+    {
+      resultado = precioGeneral - precioGeneral * porcentaje;
+    }
+    else if (opcion == 3)
+    {
+      resultado = precioGeneral + precioGeneral * porcentaje;
+    }
+    else
+    {
+      resultado = precioGeneral;
+    }
+    System.out.println("El precio de la entrada es:" + resultado);
+    s.close();
+  }
 }
