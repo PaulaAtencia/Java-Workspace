c2d231cf81a342e30f1faff7b55852ba29f611b7
PaulaAtencia <patebar593@g.educaand.es>
2023-11-08 19:40:42 +0100

    OTRO EJESISIO

--- Unidad 1-2-3-4/t5_5.java
+++ Unidad 1-2-3-4/t5_5.java
@@ -1,3 +1,10 @@
 public class t5_5 {
-    
+
+  public static void main(String[] args) {
+    int i = 320;
+    while (i >= 160) {
+      System.out.print(i + " ");
+      i = i - 20;
+    }
+  }
 }
