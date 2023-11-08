aef1505fdcd4416296d6503ac38b93269f5ac1d9
PaulaAtencia <patebar593@g.educaand.es>
2023-11-08 19:38:17 +0100

    Ejercicios hechos clase

--- Unidad 1-2-3-4/t4_28.java
+++ Unidad 1-2-3-4/t4_28.java
@@ -1,41 +1,44 @@
 import java.util.Scanner;
 
 public class t4_28 {
-
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
-    System.out.print(
-      "Turno del jugador 1 (introduzca piedra, papel o tijera): "
-    );
-    String jugada1 = s.nextLine();
-    System.out.print(
-      "Turno del jugador 1 (introduzca piedra, papel o tijera): "
-    );
-    String jugada2 = s.nextLine();
-    if (jugada1.equals(jugada2)) {
-      System.out.println("Empate");
-    } else {
-      int ganador = 2;
-      switch (jugada1) {
-        case "piedra":
-          if (jugada2.equals("tijera")) {
-            ganador = 1;
-          }
-          break;
-        case "papel":
-          if (jugada2.equals("piedra")) {
-            ganador = 1;
-          }
-          break;
-        case "tijera":
-          if (jugada2.equals("papel")) {
-            ganador = 1;
-          }
-          break;
-        default:
+    String pitufo = "";
+    String resultado = "";
+    double precioComida = 0;
+    double precioBebida = 0;
+    // Comida
+    System.out.print("¿Qué ha tomado de comer? (palmera, donut o pitufo): ");
+    String comida = s.nextLine();
+    if (comida.equalsIgnoreCase("pitufo")) {
+      System.out.print("¿Con qué se ha tomado el pitufo? (aceite o tortilla): ");
+      pitufo = s.nextLine();
+      if (pitufo.equalsIgnoreCase("aceite")) {
+        resultado = "Pitufo con aceite: 1,20 €";
+        precioComida = 1.20;
+      } else if (pitufo.equalsIgnoreCase("tortilla")) {
+        resultado = "Pitufo con tortilla: 1,60 €";
+        precioComida = 1.60;
       }
-      System.out.println("Gana el jugador " + ganador);
-      s.close();
+    } else if (comida.equalsIgnoreCase("palmera")) {
+      resultado = "Palmera: 1,40 €";
+      precioComida = 1.40;
+    } else if (comida.equalsIgnoreCase("donut")) {
+      resultado = "Donut: 1,00 €";
+      precioComida = 1.00;
     }
+    // Bebida
+    System.out.print("¿Qué ha tomado de beber? (zumo o café): ");
+    String bebida = s.nextLine();
+    if (bebida.equalsIgnoreCase("zumo")) {
+      resultado += "\nZumo: 1,50 €";
+      precioBebida = 1.50;
+    } else if (bebida.equalsIgnoreCase("café")) {
+      resultado += "\nCafé: 1,20 €";
+      precioBebida = 1.20;
+    }
+    System.out.println(resultado);
+    System.out.printf("Total desayuno: %.2f €\n", precioComida + precioBebida);
+    s.close();
   }
 }
