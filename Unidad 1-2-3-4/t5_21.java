0dcc1cece6ed6a6afab2590cf0ca18867ee91e21
PaulaAtencia <patebar593@g.educaand.es>
2023-11-08 20:45:19 +0100

    Ejercicios(08/10/23)

--- Unidad 1-2-3-4/t5_21.java
+++ Unidad 1-2-3-4/t5_21.java
@@ -1,3 +1,29 @@
 public class t5_21 {
-    
+
+  public static void main(String[] args) {
+    double dividendo = 0;
+    int contadorimp = 0;
+    double mayor = 0;
+    double impar = 0;
+    int contador = 0;
+    double num;
+    do {
+      System.out.print("Introduce un número ");
+      num = Double.parseDouble(System.console().readLine());
+      if (num % 2 != 0) {
+        impar = num;
+        dividendo += num;
+        contadorimp++;
+      } else {
+        if (num > mayor) {
+          mayor = num;
+        }
+      }
+      contador++;
+    } while (num > 0);
+    double media = (dividendo - impar) / (contadorimp - 1);
+    System.out.println("Ha introducido " + contador + " números.");
+    System.out.println("La media de los impares es: " + media + ".");
+    System.out.println("El mayor número par ha sido el: " + mayor + ".");
+  }
 }
