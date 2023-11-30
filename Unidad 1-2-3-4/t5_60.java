b3687f7de7d5c7715f44b20c415a8b6010a2612d
PaulaAtencia <patebar593@g.educaand.es>
2023-11-30 17:50:11 +0100

    cosaa

---
+++ Unidad 1-2-3-4/t5_60.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class t5_60 {
+    public static void main (String [] args){
+    Scanner s = new Scanner(System.in);
+        int 
+
+
+
+    s.close();
+    }
+}
