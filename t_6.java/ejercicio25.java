9fe432fd87cc03ed479ff1e4b7dbd62ff379c59c
PaulaAtencia <patebar593@g.educaand.es>
2023-12-13 21:01:13 +0100

    cambios

---
+++ t_6.java/ejercicio25.java
@@ -0,0 +1,27 @@
+public class ejercicio25 {
+
+  private static boolean esPrimo;
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 100; i++) {
+      int numero = (int) (Math.random() * 191) + 10;
+
+      //Para saber que el número es Primo
+      boolean esPrimo = true;
+      for (int j = 2; j < numero; j++);
+
+      int j;
+      if ((numero % j) == 0) {
+        esPrimo = false;
+      }
+    }
+    String numero;
+    if (esPrimo) {
+      System.out.print(" #" + numero + "# ");
+    } else if ((numero % 5) == 0) { // múltiplo de 5
+      System.out.print(" [" + numero + "] ");
+    } else {
+      System.out.print(" " + numero + " ");
+    }
+  } 
+}
---
+++ t_6.java/t_6/java/ejercicio27.java
@@ -0,0 +1,24 @@
+package t_6.java;
+
+public class ejercicio27 {
+    public static void main (String []args){
+        Scanner s= new Scanner (System.in);
+
+        System.out.println("Turno del jugador (introduzca piedra, papel o tijera)");
+        String jugador= 
+
+
+
+
+
+
+
+
+
+
+
+
+        s.close();
+    }
+
+}
