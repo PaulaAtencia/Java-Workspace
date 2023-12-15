e4948b5663315db8b7cd52f10882283c835112c5
PaulaAtencia <patebar593@g.educaand.es>
2023-12-15 18:14:09 +0100

    subidooo

--- t_7.java/t7_3.java
+++ t_7.java/t7_3.java
@@ -1,12 +1,17 @@
 public class t7_3 {
     public static void main (String[]args){
-
-
-
-
-
-
+    
+        int[]matriz =new int[10];
         
-    }
+        for (int i = 0; i < 10; i++){
 
-}
+          System.out.print("Introduce el valor del array: ");
+          matriz[i] = Integer.parseInt(System.console().readLine());
+        }
+        System.out.println();
+        for (int i = 9; i >= 0; i--){
+          System.out.print(matriz[i]+" ");
+        }
+      }
+    }
+        
