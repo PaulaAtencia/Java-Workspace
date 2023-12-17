d3f58c8d492ff25d326a031be192bf4283c070e1
PaulaAtencia <patebar593@g.educaand.es>
2023-12-17 14:12:21 +0100

    comentarioz

---
+++ t_7.java/t5_9.java
@@ -0,0 +1,20 @@
+public class t5_9 {
+    public static void main(String[]args){
+
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- t_7.java/t7_4.java
+++ t_7.java/t7_4.java
@@ -1,28 +1,40 @@
 public class t7_4 {
     public static void main (String [] args){
 
+      //Preparo los arrays
+
         int[] numero = new int[20];
         int[] cuadrado = new int[20];
         int[] cubo = new int[20];
+
+        //Relleno con random el pimero
         for (int i = 0; i < 20; i++){
           numero[i] = (int)(Math.random()*100);
         }
+
+        //Relleno el array de cuadrado
         for (int i = 0; i < 20; i++){
           cuadrado[i] = numero[i]*numero[i];
         }
+
+        //Relleno el array de cubo
         for (int i = 0; i < 20; i++){
           cubo[i] = cuadrado[i]*numero[i];
         }
+
+        //Se mueta los arrays
         System.out.println();
         System.out.print("Número: ");
         for (int i = 0; i < 20; i++){
           System.out.print(numero[i]+" ");
         }
+
         System.out.println();
         System.out.print("Cuadrado: ");
         for (int i = 0; i < 20; i++){
           System.out.print(cuadrado[i]+" ");
         }
+        
         System.out.println();
         System.out.print("Cubo: ");
         for (int i = 0; i < 20; i++){
